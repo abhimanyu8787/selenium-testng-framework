c89ae44f7be3dbf80215276bfda2ddee70d075ae
Abhimanyu <abhimanyukumar8787@gmail.com>
2023-04-11 23:37:33 +0530

    Modified base test to accomodate remote webdriver

--- src/test/java/automation/utils/BaseTest.java
+++ src/test/java/automation/utils/BaseTest.java
@@ -6,27 +6,27 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeMethod;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import automation.genericfunctions.Utility;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -35,36 +35,49 @@ public class BaseTest {
 
     public WebDriver driver;
 
-    public WebDriver initializeDriver() throws IOException
+    public WebDriver initializeDriver() throws Exception{
+        ExecutionMode executionMode = ExecutionMode.valueOf(Utility.getGlobalProperties("ExecutionMode"));
+        switch (executionMode) {
+        case Local: {
+            String browserName = Utility.getGlobalProperties("Browser");
 
-    {
-        String browserName = Utility.getGlobalProperties("Browser");
+            if (browserName.contains("chrome")) {
+                WebDriverManager.chromedriver().setup();
+                String[] chromeConfig = { "--ignore-certificate-errors", "--ignore-ssl-errors=yes", "--no-sandbox",
+                        "start-maximized" };
+                String userDir = System.getProperty("user.dir");
+                String downloadPath = userDir + "\\src\\test\\resources\\downloads";
+                System.out.println("Download path is being set to: " + downloadPath);
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments(chromeConfig);
+                options.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
+                Map<String, Object> prefs = new HashMap<String, Object>();
+                prefs.put("credentials_enable_service", false);
+                prefs.put("profile.password_manager_enabled", false);
+                prefs.put("profile.default_content_settings.popups", 0);
+                prefs.put("download.default_directory", downloadPath);
+                options.setExperimentalOption("prefs", prefs);
 
-        if (browserName.contains("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            String[] chromeConfig = { "--ignore-certificate-errors", "--ignore-ssl-errors=yes", "--no-sandbox",
-                    "start-maximized" };
-            String userDir = System.getProperty("user.dir");
-            String downloadPath = userDir + "\\src\\test\\resources\\downloads";
-            System.out.println("Download path is being set to: " + downloadPath);
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments(chromeConfig);
-            options.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
-            Map<String, Object> prefs = new HashMap<String, Object>();
-            prefs.put("credentials_enable_service", false);
-            prefs.put("profile.password_manager_enabled", false);
-            prefs.put("profile.default_content_settings.popups", 0);
-            prefs.put("download.default_directory", downloadPath);
-            options.setExperimentalOption("prefs", prefs);
+                driver = new ChromeDriver(options);
 
-            driver = new ChromeDriver(options);
-
-        } else if (browserName.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-        } else if (browserName.equalsIgnoreCase("edge")) {
-            WebDriverManager.edgedriver().setup();
-            driver = new EdgeDriver();
+            } else if (browserName.equalsIgnoreCase("firefox")) {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            } else if (browserName.equalsIgnoreCase("edge")) {
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+            }
+            break;
+        }
+        case Remote: {
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.setBrowserName("chrome");
+            cap.setPlatform(Platform.LINUX);
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
+            break;
+        }
+        default:
+            throw new Exception("Unhandled Execution Mode!");
         }
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
@@ -81,12 +94,12 @@ public class BaseTest {
         return System.getProperty("user.dir") + "//reports//" + testCaseName + ".png";
 
     }
-    
+
     @BeforeMethod
-    public void getWebDriver() throws IOException {
+    public void getWebDriver() throws Exception {
         driver = initializeDriver();
     }
-    
+
     @AfterMethod
     public void tearDown() {
         driver.close();
