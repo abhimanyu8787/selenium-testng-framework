d786518317390bc99b60d5d4b978ba0c43fefc30
Abhimanyu <abhimanyukumar8787@gmail.com>
2023-04-15 18:00:05 +0530

    Added code for selenium grid remote webdriver

--- src/test/java/automation/utils/BaseTest.java
+++ src/test/java/automation/utils/BaseTest.java
@@ -35,7 +35,7 @@ public class BaseTest {
 
     public WebDriver driver;
 
-    public WebDriver initializeDriver() throws Exception{
+    public WebDriver initializeDriver() throws Exception {
         ExecutionMode executionMode = ExecutionMode.valueOf(Utility.getGlobalProperties("ExecutionMode"));
         switch (executionMode) {
         case Local: {
@@ -76,6 +76,36 @@ public class BaseTest {
             driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
             break;
         }
+        case SeleniumGrid: {
+            String host = Utility.getGlobalProperties("Host");
+            String port = Utility.getGlobalProperties("Port");
+            String browser = Utility.getGlobalProperties("NodeBrowser");
+
+            String completeUrl = "http://" + host + ":" + port + "/wd/hub";
+            System.out.println("Selenium Hub URL: "+completeUrl);
+
+            if (browser.equalsIgnoreCase("Chrome")) {
+                String[] chromeConfig = { "--ignore-certificate-errors", "--ignore-ssl-errors=yes", "--no-sandbox",
+                        "start-maximized" };
+                String userDir = System.getProperty("user.dir");
+                String downloadPath = userDir + "\\src\\test\\resources\\downloads";
+                System.out.println("Download path is being set to: " + downloadPath);
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments(chromeConfig);
+                options.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
+                Map<String, Object> prefs = new HashMap<String, Object>();
+                prefs.put("credentials_enable_service", false);
+                prefs.put("profile.password_manager_enabled", false);
+                prefs.put("profile.default_content_settings.popups", 0);
+                prefs.put("download.default_directory", downloadPath);
+                options.setExperimentalOption("prefs", prefs);
+                options.setCapability("browserName", "chrome");
+                options.setCapability("platformName", "linux");
+                driver = new RemoteWebDriver(new URL(completeUrl), options);
+                break;
+            }
+
+        }
         default:
             throw new Exception("Unhandled Execution Mode!");
         }
@@ -102,7 +132,7 @@ public class BaseTest {
 
     @AfterMethod
     public void tearDown() {
-        driver.close();
+        driver.quit();
     }
 
 }
